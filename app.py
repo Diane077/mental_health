32934fe6eea0a0376530bf9ad8db9dfbd44a8d26
Diane Munsah <dianemunsah57@gmail.com>
2024-06-22 11:31:07 +0200

    done

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
+
+
+import numpy as np
+import pandas as pd
 st.write('Hello Arvind')
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
